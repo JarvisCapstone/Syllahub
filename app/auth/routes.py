6d465ca60f7b1e0671866b08a897ea43ffe8997d
Shahab <ssalehi@kent.edu>
2019-03-14 17:48:56 +0000

    pushing Nicks fixes

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, url_for, flash, request
-from flask_login import login_user, logout_user, current_user
+from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
 
 from app import db
@@ -15,6 +15,7 @@ def login():
     form = LoginForm()
 
     if form.validate_on_submit():
+        print("validated on submit")
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
@@ -22,13 +23,14 @@ def login():
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('main.index')
-        print("validated")
+            next_page = url_for('home.index')
+
         return redirect(next_page)
     print("not validated")
     return render_template('auth/login.html', title='Login', form=form)
 
 @bp.route('/logout')
+@login_required
 def logout():
     logout_user()
     return redirect(url_for('home.index'))
@@ -54,4 +56,8 @@ def register():
                            form=form)
 
 
-
+@bp.route('/my_profile')
+@login_required
+def my_profile():
+    return "my_profile"
+    #return redirect(url_for('auth.index'))
