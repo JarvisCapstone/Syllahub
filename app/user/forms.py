69890743395c9ab3d18db55317ada3103873b5b3
krichardson864 <kricha36@kent.edu>
2019-03-19 19:44:47 -0400

    working on user forms

---
+++ app/user/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import User
+
+class DeleteUserForm(FlaskForm):
+    username = StringField('Email', validators=[DataRequired()])
+    submit = SubmitField('Delete User')
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
