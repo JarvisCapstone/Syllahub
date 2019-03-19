b578efed806ea94d5ee7d2c7e0b3ee3a59125428
NickStafford2 <41924040+NickStafford2@users.noreply.github.com>
2019-03-19 19:39:41 -0400

    quality of life changes

--- flaskApp.py
+++ flaskApp.py
@@ -2,7 +2,7 @@ from app import create_app, db
 
 app = create_app()
 
-print("db in flaskapp.py=", db)
+#print("db in flaskapp.py=", db)
 
 def view_routes():
     for name, func in app.view_functions.items():
