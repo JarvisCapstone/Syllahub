50bead6b5918c560722e6f8713586f7d6b6231ee
NickStafford2 <41924040+NickStafford2@users.noreply.github.com>
2019-04-02 22:54:34 -0400

    updated flask shell imports

--- flaskApp.py
+++ flaskApp.py
@@ -1,17 +1,25 @@
 from app import create_app, db
 from app.models import *
+import factory
+
 app = create_app()
 
 #print("db in flaskapp.py=", db)
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 
-            'User': User, 
-            'Instructor': Instructor,
-            'Syllabus': Syllabus,
-            'SyllabusInstructorAssociation': SyllabusInstructorAssociation
-            }
+    return {
+        'db': db, 
+
+        'Instructor': Instructor,
+        'Clo': Clo,
+        'Course': Course,
+        'Syllabus': Syllabus,
+        'SyllabusInstructorAssociation': SyllabusInstructorAssociation,
+        'User': User, 
+
+        'factory': factory, 
+    }
 
 def view_routes():
     for name, func in app.view_functions.items():
