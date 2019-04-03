afae1bab33bdf8e0ef705ce7b3f265662b99fe7f
krichardson864 <kricha36@kent.edu>
2019-04-03 17:14:06 -0400

    successful create course form

--- app/course/routes.py
+++ app/course/routes.py
@@ -1,6 +1,9 @@
 from app.course import bp
-from flask import render_template
+from flask import render_template, flash
 from flask_login import current_user, login_required
+from app.models import Course
+from app.course.forms import createCourseForm
+from app import db
 
 @bp.route('/index', methods=['GET', 'POST'])
 def index():
@@ -10,7 +13,22 @@ def index():
 
 @bp.route('/create', methods=['GET', 'POST'])
 def create():
-    return render_template('course/create.html')
+    form = createCourseForm()
+    if form.validate_on_submit():
+        course = Course(number = int(form.courseNumber.data), version = int(form.courseVersion.data), 
+                        name = form.courseName.data, description = form.courseDescription.data, 
+                        prerequisites = form.coursePrereqs.data, building = form.courseBuilding.data, 
+                        room = form.courseRoomNo.data)
+      
+        course.is_core = form.isCore.data
+        course.is_diversity = form.isDiversity.data
+        course.is_elr = form.isELR.data
+        course.is_wi = form.isWI.data
+
+        db.session.add(course)
+        db.session.commit()
+        flash("Course Created!")
+    return render_template('course/create.html', title="Create Course", form=form)
 
 
 
