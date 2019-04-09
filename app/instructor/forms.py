f455683a0b37a328452114ee976cb57b19690931
Jess Gehring <34904627+JessG96@users.noreply.github.com>
2019-04-09 17:00:15 -0400

    Create, Delete and Index are working.

--- app/instructor/forms.py
+++ app/instructor/forms.py
@@ -4,8 +4,15 @@ from wtforms.validators import ValidationError, DataRequired, Email
 from app.models import Instructor
 
 class createInstructorForm(FlaskForm):
-    name = StringField('InstructorName')
-    phone = IntegerField('PhoneNumber')
-    email = StringField('Email')
+    name = StringField('InstructorName', validators=[DataRequired()])
+    phone = IntegerField('PhoneNumber', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
     hours = StringField('OfficeHours')
     submit = SubmitField('Submit')
+
+class deleteInstructorForm(FlaskForm):
+    id = IntegerField('Id')
+    submit = SubmitField('Submit')
+
+class readInstructorForm(FlaskForm):
+    id = IntegerField('Id', validators=[DataRequired])
--- app/instructor/routes.py
+++ app/instructor/routes.py
@@ -1,24 +1,36 @@
 from app.instructor import bp
-from app.instructor.forms import createInstructorForm
-from flask import render_template
+from app.models import Instructor
+from flask import render_template, jsonify, flash, redirect, url_for
 from flask_login import current_user, login_required
+from app import db
+from app.instructor.forms import createInstructorForm, deleteInstructorForm, readInstructorForm
 
 @bp.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('instructor/index.html')
+    instructors = Instructor.query.all()
+    print(instructors)
+
+    return render_template('instructor/index.html', instructors=instructors)
 
 
 
 @bp.route('/create', methods=['GET', 'POST'])
 def create():
     form = createInstructorForm()
-    return render_template('instructor/create.html', form=form)
-
+    if form.validate_on_submit():
+        instructor = Instructor(name=form.name.data, phone=form.phone.data, email=form.email.data, perfered_office_hours=form.hours.data)
+        db.session.add(instructor)
+        db.session.commit()
+    return render_template('instructor/create.html', title="Create Instructor", form=form)
 
 
 @bp.route('/read/<id>', methods=['GET', 'POST'])
 def read(id):
-    return render_template('/instructor/read.html')
+    form = readInstructorForm()
+    if form.validate_on_submit():
+        instructor = Instructor.query.filter_by(id = int(form.id.data)).one()
+        flash("Instructor read!")
+    return render_template('/instructor/read.html', title="Instructor read!", form=form)
 
 
 
@@ -27,7 +39,15 @@ def update(id):
     return render_template('/instructor/update.html')
 
 
-
 @bp.route('/delete/<id>', methods=['GET', 'POST'])
 def delete(id):
-    return render_template('/instructor/delete.html')
+    form = deleteInstructorForm()
+    if form.validate_on_submit():
+        instructor = Instructor.query.filter_by(id=form.id.data).first_or_404()
+        print(instructor)
+        db.session.delete(instructor)
+        db.session.commit()
+
+    return render_template('/instructor/delete.html', form=form)
+
+     
