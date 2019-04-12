dc74e4d2ecfba96a0e1329e6d0142bda6e2e9a99
NickStafford2 <41924040+NickStafford2@users.noreply.github.com>
2019-04-11 21:42:05 -0400

    abstract factory half finished

--- factory.py
+++ factory.py
@@ -1,7 +1,7 @@
-'''creates fake models and adds them to the database. 
+'''Creates fake models and adds them to the database. 
 
 call commands like these from the flask shell
->>>factory.addFakeUserToDB()
+>>>TODO
 
 '''
 from app import db
@@ -11,6 +11,8 @@ from sqlalchemy.sql.expression import func
 from faker import Faker
 from faker.providers import profile, phone_number, lorem, address, misc
 import random
+from abc import ABCMeta, abstractmethod
+from typing import List, Set, Dict, Tuple, Optional
 
 fake = Faker() 
 fake.add_provider(profile)
@@ -20,389 +22,266 @@ fake.add_provider(misc)
 fake.add_provider(address)
 
 
-class Factory:
-    def __init__(self, model):
-        self.model = model
-        self.data = []
-
-    def setDefaultData():
+class Factory(metaclass=ABCMeta):
+    @abstractmethod
+    def getModel(self): 
         pass
 
-    def create():
+    @abstractmethod
+    def setData(self, temp, data): 
         pass
 
-    def addToDB(num=None):
-        if not num: 
-            num = 3
-        else:
-            num = int(num)
-        return "TODO"
+    def create(self, data=None):
+        temp = self.getModel()
+        tempData = data or {}
+        temp = self.setData(temp, tempData)
+        return temp
+
+    def addToDB(self, num=None):
+        num = int(num) or 1
+        u = self.create()
+        db.session.add(u)
+        db.session.commit()
+        return "Success"
+
 
 
 class UserFactory(Factory):
     def __init__(self):
-        Factory.__init__(self, User)
-
-
-def createFakeUser(username=None, email=None, password=None, permission=None, instructor_id=None):
-    p = fake.simple_profile()
-    u = User()
-    if username:
-        u.username=username
-    else:
-        u.username=p['username']
+        Factory.__init__(self)
     
-    if email:
-        u.email=email
-    else:
-        u.email=p['mail']    
+    def getModel(self):
+        return User()
+
+    def setData(self, temp, data): 
+        p = fake.simple_profile()
+        if 'email' in data:
+            temp.email = data.email
+        else:
+            temp.email = p['mail']
+
+        if 'password' in data:
+            temp.set_password(data.password)
+        else:
+            #password is 'password by default'
+            temp.set_password('password')
+
+        if 'permission' in data:
+            temp.permission=data.permission
+        
+        if 'instructor_id' in data:
+            temp.instructor_id=data.instructor_id
+
+        return temp
+
+
+class InstructorFactory(Factory):
+    def __init__(self):
+        Factory.__init__(self)
     
-    if password:
-        u.set_password(password)
-    else:
-        #password is 'password by default'
-        u.set_password('password')
+    def getModel(self):
+        return Instructor()
 
-    if permission:
-        u.permission=permission
+    def setData(self, temp, data): 
+        if 'name' in data:
+            temp.name=data.name
+        else:
+            temp.name=p['name']
+        
+        if 'phone' in data:
+            temp.phone=data.phone
+        else:
+            temp.phone=random.randint(1000000000,9999999999)   
+
+        if 'email' in data:
+            temp.email=data.email
+        else:
+            temp.email=p['mail']
+
+        
+        if 'perfered_office_hours' in data:
+            temp.perfered_office_hours=data.perfered_office_hours
+        else:
+            temp.perfered_office_hours='whenever'
+
+        return temp
+
+
+
+class CourseFactory(Factory):
+    def __init__(self):
+        Factory.__init__(self)
+
+    def getModel(self):
+        return Course()
+
+    def setData(self, temp, data): 
+        if 'number' in data:
+            temp.number=data.number
+        else:
+            temp.number=fake.random_int(min=10000, max=99999)
+        
+        if 'version' in data:
+            temp.version=data.version
+        else:
+            temp.version=fake.random_int(min=1, max=9)
+
+        if 'name' in data:
+            temp.name=data.name
+        else:
+            temp.name=fake.sentence(nb_words=3)
+
+        if 'description' in data:
+            temp.description=data.description
+        else:
+            temp.description=fake.paragraph(nb_sentences=2)
+
+        if 'prerequisites' in data:
+            temp.prerequisites=data.prerequisites
+        else:
+            temp.prerequisites=fake.sentence(nb_words=1)
+
+        if 'building' in data:
+            temp.building=data.building
+        else:
+            temp.building=fake.street_address()
+
+        if 'room' in data:
+            temp.room=data.room
+        else:
+            temp.room=fake.random_int(min=0, max=999)
+
+        if 'is_core' in data:
+            temp.is_core=data.is_core
+        else:
+            temp.is_core=fake.boolean(chance_of_getting_true=20)
+
+        if 'is_wi' in data:
+            temp.is_wi=data.is_wi
+        else:
+            temp.is_wi=fake.boolean(chance_of_getting_true=20)
+
+        if 'is_elr' in data:
+            temp.is_elr=data.is_elr
+        else:
+            temp.is_elr=fake.boolean(chance_of_getting_true=20)
+
+        if 'is_diversity' in data:
+            temp.is_diversity=data.is_diversity
+        else:
+            temp.is_diversity=fake.boolean(chance_of_getting_true=20)
+
+        return temp
+
+class CloFactory(Factory):
+    def __init__(self):
+        Factory.__init__(self, Clo)
+
     
-    if instructor_id:
-        u.instructor_id=instructor_id
+    def getModel(self):
+        return Clo()
 
-    return u
+    def create2(general=None, specific=None):
+        u = Clo()
 
-def addFakeUserToDB(username=None, email=None, password=None, permission=None, instructor_id=None):
-    u = createFakeUser(username, email, password, permission, instructor_id)
-    db.session.add(u)
-    db.session.commit()
+        u.general = general or fake.paragraph(nb_sentences=2)
+        u.specific = specific or fake.paragraph(nb_sentences=2)
+
+        return u
 
 
 
-def createFakeInstructor(name=None, phone=None, email=None, perfered_office_hours=None):
-    p = fake.simple_profile()
-    u = Instructor()
-    if name:
-        u.name=name
-    else:
-        u.name=p['name']
+class SyllabusFactory(Factory):
+    def __init__(self):
+        Factory.__init__(self)
     
-    if phone:
-        u.phone=phone
-    else:
-        u.phone=random.randint(1000000000,9999999999)   
-
-    if email:
-        u.email=email
-    else:
-        u.email=p['mail']
-
-    
-    if perfered_office_hours:
-        u.perfered_office_hours=perfered_office_hours
-    else:
-        u.perfered_office_hours='whenever'
-
-    return u
-
-def addFakeInstructorToDB(name=None, phone=None, email=None, perfered_office_hours=None):
-    u = createFakeInstructor(name, phone, email, perfered_office_hours)
-    db.session.add(u)
-    db.session.commit()
-
-
-
-def createFakeCourse(number=None, version=None, name=None, description=None, 
-                     prerequisites=None, building=None, room=None, 
-                     is_core=None, is_wi=None, is_elr=None, is_diversity=None):   
-    u = Course()
-    if number:
-        u.number=number
-    else:
-        u.number=fake.random_int(min=10000, max=99999)
-    
-    if version:
-        u.version=version
-    else:
-        u.version=fake.random_int(min=1, max=9)
-
-    if name:
-        u.name=name
-    else:
-        u.name=fake.sentence(nb_words=3)
-
-    if description:
-        u.description=description
-    else:
-        u.description=fake.paragraph(nb_sentences=2)
-
-    if prerequisites:
-        u.prerequisites=prerequisites
-    else:
-        u.prerequisites=fake.sentence(nb_words=1)
-
-    if building:
-        u.building=building
-    else:
-        u.building=fake.street_address()
-
-    if room:
-        u.room=room
-    else:
-        u.room=fake.random_int(min=0, max=999)
-
-    if is_core:
-        u.is_core=is_core
-    else:
-        u.is_core=fake.boolean(chance_of_getting_true=20)
-
-    if is_wi:
-        u.is_wi=is_wi
-    else:
-        u.is_wi=fake.boolean(chance_of_getting_true=20)
-
-    if is_elr:
-        u.is_elr=is_elr
-    else:
-        u.is_elr=fake.boolean(chance_of_getting_true=20)
-
-    if is_diversity:
-        u.is_diversity=is_diversity
-    else:
-        u.is_diversity=fake.boolean(chance_of_getting_true=20)
-
-    return u
-
-def addFakeCourseToDB(number=None, version=None, name=None, description=None, 
-                     prerequisites=None, building=None, room=None, 
-                     is_core=None, is_wi=None, is_elr=None, is_diversity=None):
-    u = createFakeCourse(number, version, name, description, 
-                         prerequisites, building, room, 
-                         is_core, is_wi, is_elr, is_diversity)
-    db.session.add(u)
-    db.session.commit()
-
-
-
-def createFakeClo(general=None, specific=None):
-    u = Clo()
-
-    if general:
-        u.general=general
-    else:
-        u.general=fake.paragraph(nb_sentences=2)
-
-    if specific:
-        u.specific=specific
-    else:
-        u.specific=fake.paragraph(nb_sentences=2)
-
-    return u
-
-def addFakeCloToDB(general=None, specific=None): 
-    u = createFakeClo(general, specific)
-    db.session.add(u)
-    db.session.commit()
-
-
-
-def createFakeSyllabus(section=None, semester=None, year=None, version=None, 
-                       course_number=None, course_version=None, state=None, 
-                       pdf=None, calender=None, schedule=None, 
-                       required_materials=None, optional_materials=None, 
-                       withdrawl_date=None, grading_policy=None, 
-                       attendance_policy=None, cheating_policy=None, 
-                       extra_policies=None, meeting_time=None, 
-                       meeting_dates=None, University_cheating_policy=None, 
-                       Students_with_disabilities=None):
-    #must reference a course. get a random course
-
-    course = Course.query.order_by(func.rand()).first()
-
-    u = Syllabus()
-    if section:
-        u.section = section
-    else: 
-        u.section = fake.random_int(min=0, max=50)
-
-
-    if semester:
-        u.semester = semester
-    else: 
-        u.semester = 'spring'
-
-
-    if year:
-        u.year = year
-    else: 
-        u.year = 2018
-
-
-    if version:
-        u.version = version
-    else: 
-        u.version = fake.random_int(min=1, max=9)
-
-
-    if course_number:
-        u.course_number = course_number
-    else: 
-        u.course_number = course.number
-
-
-    if course_version:
-        u.course_version = course_version
-    else: 
-        u.course_version = course.version
-
-
-    if state:
-        u.state = state
-    else: 
-        u.state = 'draft'
-
-
-    if pdf:
-        u.pdf = pdf
-
-
-    if calender:
-        u.calender = calender
-
-
-    if schedule:
-        u.schedule = schedule
-
-
-    if required_materials:
-        u.required_materials = required_materials
-    else: 
-        u.required_materials = fake.paragraph(nb_sentences=2)
-
-
-    if optional_materials:
-        u.optional_materials = optional_materials
-    else: 
-        u.optional_materials = fake.paragraph(nb_sentences=2)
-
-
-    if withdrawl_date:
-        u.withdrawl_date = withdrawl_date
-    else: 
-        u.withdrawl_date = fake.paragraph(nb_sentences=1)
-
-
-    if grading_policy:
-        u.grading_policy = grading_policy
-    else: 
-        u.grading_policy = fake.paragraph(nb_sentences=3)
-
-
-    if attendance_policy:
-        u.attendance_policy = attendance_policy
-    else: 
-        u.attendance_policy = fake.paragraph(nb_sentences=3)
-
-
-    if cheating_policy:
-        u.cheating_policy = cheating_policy
-    else: 
-        u.cheating_policy = fake.paragraph(nb_sentences=3)
-
-
-    if extra_policies:
-        u.extra_policies = extra_policies
-    else: 
-        u.extra_policies = fake.paragraph(nb_sentences=3)
-
-
-    if meeting_time:
-        u.meeting_time = meeting_time
-    else: 
-        u.meeting_time = fake.paragraph(nb_sentences=1)
-
-
-    if meeting_dates:
-        u.meeting_dates = meeting_dates
-    else: 
-        u.meeting_dates = fake.paragraph(nb_sentences=1)
-
-
-    if University_cheating_policy:
-        u.University_cheating_policy = University_cheating_policy
-    else: 
-        u.University_cheating_policy = fake.paragraph(nb_sentences=3)
-
-
-    if Students_with_disabilities:
-        u.Students_with_disabilities = Students_with_disabilities
-    else: 
-        u.Students_with_disabilities = fake.paragraph(nb_sentences=3)
-
-    return u
-
-def addFakeSyllabusToDB(section=None, semester=None, year=None, version=None, 
-                        course_number=None, course_version=None, state=None, 
-                        pdf=None, calender=None, schedule=None, 
-                        required_materials=None, optional_materials=None, 
-                        withdrawl_date=None, grading_policy=None, 
-                        attendance_policy=None, cheating_policy=None, 
-                        extra_policies=None, meeting_time=None, 
-                        meeting_dates=None, University_cheating_policy=None, 
-                        Students_with_disabilities=None): 
-    u = createFakeSyllabus(section, semester, year, version, course_number, 
-                           course_version, state, pdf, calender, schedule, 
-                           required_materials, optional_materials, 
-                           withdrawl_date, grading_policy, attendance_policy, 
-                           cheating_policy, extra_policies, meeting_time, 
-                           meeting_dates, University_cheating_policy, 
-                           Students_with_disabilities)
-    db.session.add(u)
-    db.session.commit()
-
-
-def createRandCloCourseAssociation():
+    def getModel(self):
+        return Syllabus()
+
+    def create2(section=None, semester=None, year=None, version=None, 
+                           course_number=None, course_version=None, state=None, 
+                           pdf=None, calender=None, schedule=None, 
+                           required_materials=None, optional_materials=None, 
+                           withdrawl_date=None, grading_policy=None, 
+                           attendance_policy=None, cheating_policy=None, 
+                           extra_policies=None, meeting_time=None, 
+                           meeting_dates=None, University_cheating_policy=None, 
+                           Students_with_disabilities=None):
+        #must reference a course. get a random course
+        course = Course.query.order_by(func.rand()).first()
+
+        u = Syllabus()
+        u.section = section or fake.random_int(min=0, max=50)
+        u.semester = semester or 'spring'
+        u.year = year or 2018
+        u.version = version or fake.random_int(min=1, max=9)
+        u.course_number = course_number or course.number
+        u.course_version = course_version or course.version
+        u.state = state or 'draft'
+
+        if pdf:
+            u.pdf = pdf
+
+        if calender:
+            u.calender = calender
+
+        if schedule:
+            u.schedule = schedule
+
+        u.required_materials = required_materials or fake.paragraph(nb_sentences=2)
+        u.optional_materials = optional_materials or fake.paragraph(nb_sentences=2)
+        u.withdrawl_date = withdrawl_date or fake.paragraph(nb_sentences=1)
+        u.grading_policy = grading_policy or fake.paragraph(nb_sentences=3)
+        u.attendance_policy = attendance_policy or fake.paragraph(nb_sentences=3)
+        u.cheating_policy = cheating_policy or fake.paragraph(nb_sentences=3)
+        u.extra_policies = extra_policies or fake.paragraph(nb_sentences=3)
+        u.meeting_time = meeting_time or fake.paragraph(nb_sentences=1)
+        u.meeting_dates = meeting_dates or fake.paragraph(nb_sentences=1)
+        u.University_cheating_policy = University_cheating_policy or fake.paragraph(nb_sentences=3)
+        u.Students_with_disabilities = Students_with_disabilities or fake.paragraph(nb_sentences=3)
+
+        return u
+
+
+
+def create2RandCloCourseAssociation():
     temp_course = Course.query.order_by(func.rand()).first()
     temp_clo = Clo.query.order_by(func.rand()).first()
     temp_course.clos.append(temp_clo)
     db.session.commit()
 
 
-def printRandInstructorSyllabi():
-    i = Instructor.query.order_by(func.rand()).first()
-    print(i.syllabiList)
-    print(i.all_syllabus_associations)
-
-
-def printRandSyllabusInstructors():
-    temp_syllabus = Syllabus.query.order_by(func.rand()).first()
-    print(temp_syllabus.instructorList)
-    print(temp_syllabus.all_instructor_associations)
-
-
-def createRandInstructorSyllabusAssociation():
+def create2RandInstructorSyllabusAssociation():
     temp_syllabus = Syllabus.query.order_by(func.rand()).first()
     temp_instructor = Instructor.query.order_by(func.rand()).first()
-
-    SyllabusInstructorAssociation.create(temp_syllabus, temp_instructor, 'grader')
+    SyllabusInstructorAssociation.create2(temp_syllabus, temp_instructor, 'grader')
 
 
 def generateData(num=None):
     '''seed db with junk data
     Warning: does not check for primary keys. 
-    may randomly create existing data and crash
+    may randomly create2 existing data and crash
     '''
     if not num: 
         num = 3
     else:
         num = int(num)
 
+    factories = []
+    u = UserFactory()
+    factories.append(u)
+    print(factories)
+    for factory in factories:
+        x = factory.create()
+        print("generated x=", x)
+    '''
     for x in range(num):
         addFakeCourseToDB()
         addFakeInstructorToDB()
         addFakeUserToDB()
         addFakeCloToDB()
         addFakeSyllabusToDB()
-        createRandCloCourseAssociation()
-        createRandInstructorSyllabusAssociation()
-
+        create2RandCloCourseAssociation()
+        create2RandInstructorSyllabusAssociation()
+    '''
     print("added", num, "fake data entries to each table in db")
