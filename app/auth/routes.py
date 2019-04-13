dd35f5c515e0d8465e6d0e95bdaac7f360c64692
krichardson864 <kricha36@kent.edu>
2019-04-13 16:00:48 -0400

    From the admin profile page, an admin can now make a course, make an instructor and then, assign an existing instructor to an existing course, however that also requires an existing syllabus

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -5,8 +5,8 @@ from werkzeug.urls import url_parse
 
 from app import db
 from app.auth import bp
-from app.auth.forms import LoginForm, RegistrationForm
-from app.models import User
+from app.auth.forms import LoginForm, RegistrationForm, assignInstructorToCourse
+from app.models import User, SyllabusInstructorAssociation
 
 
 @bp.route('/login', methods=['GET', 'POST'])
@@ -61,6 +61,24 @@ def my_profile():
     else:
         return render_template('auth/instructor_profile.html')
     #return redirect(url_for('auth.index'))
+
+@bp.route('/assignInstruct', methods=['GET', 'POST'])
+@login_required
+def assignInstruct():
+    form = assignInstructorToCourse()
+    if form.validate_on_submit():
+        relationship = SyllabusInstructorAssociation(syllabus_course_number = int(form.courseNumber.data), 
+                                                    syllabus_course_version = int(form.courseVersion.data), 
+                                                    syllabus_semester = form.semester.data, 
+                                                    syllabus_year = form.year.data, 
+                                                    syllabus_version = form.syllabusVersion.data, 
+                                                    instructor_id = form.instructorID.data,
+                                                    syllabus_section = form.courseSection.data)
+        db.session.add(relationship)
+        db.session.commit()
+        flash("Assignment Made!")
+        return redirect(url_for('auth.my_profile'))
+    return render_template('auth/assignInstruct.html', form=form)
 
 
 
