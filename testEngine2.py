99be2d6ce4e239e5424b595b3e37ba0b327d3006
NickStafford2 <41924040+NickStafford2@users.noreply.github.com>
2019-04-30 22:33:45 -0400

    remove testEngine from remote repository

--- testEngine2.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-engine = create_engine('mysql+pymysql://root:r9qi8nPF@127.0.0.1/Syllahub')
-conn = engine.connect()
-result = conn.execute("select email from user")
-
-for row in result:
-    print(row['email'])
